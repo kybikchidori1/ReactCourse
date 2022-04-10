5e24550e2bd786ddca9eba2748a04712b4bf05ca
Roman <kybik_rybik1994@mail.ru>
2022-04-10 21:36:42 +0300

    practice 7

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Header />
+      <Technologies />
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
       <ul>
         <li>Компонента - это функция, возвращающася разметку JSX </li>
         <li>Компонента - это тег</li>
         <li>dsds</li>
         <li>dsds</li>
       </ul>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News Feed</a>
+      <a href="#s">Messages</a>
     </div>
   );
 };
