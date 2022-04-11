571c64f71c2d97e47a68039523fc4e32c065d10e
Roman <kybik_rybik1994@mail.ru>
2022-04-12 00:59:40 +0300

    12.04.2022

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header>
+        <img src="https://i.pinimg.com/originals/f6/7a/3a/f67a3a261f0c952640f9c1d214d73e96.png" />
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div>Main Content</div>
     </div>
   );
 };
-
 
 export default App;
