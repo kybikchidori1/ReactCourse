6ee732b95cfc8257cafc4c19de7939b112dc19ae
Roman <kybik_rybik1994@mail.ru>
2022-04-17 22:10:49 +0300

    end practice 19

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
+// import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="app-wrapper">
       <Header />
-      <Navbar/>
-      <Profile/>
-    </div>);
+      <Navbar />
+      {/* <Profile/> */}
+      <div className='app-wrapper-content'>
+        <Dialogs />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import s from './Profile.module.css';
+import s from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return <div className={s.content}>
+  return (
     <div>
-      <img src="https://avatars.mds.yandex.net/get-zen_doc/3403845/pub_60d2e9de68a7aa40c2fc262e_60d2f399d6f4ab7d154dfa30/scale_1200" />
-    </div>
-    <div>ava + description</div>
+      <div>
+        <img src="https://avatars.mds.yandex.net/get-zen_doc/3403845/pub_60d2e9de68a7aa40c2fc262e_60d2f399d6f4ab7d154dfa30/scale_1200" />
+      </div>
+      <div>ava + description</div>
       <MyPosts />
-  </div>
-}
+    </div>
+  );
+};
 
 export default Profile;
