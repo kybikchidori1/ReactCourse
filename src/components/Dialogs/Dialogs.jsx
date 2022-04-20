0332718cbb2d10dd102b37aee8887c86c87c02ff
Roman <kybik_rybik1994@mail.ru>
2022-04-20 21:38:55 +0300

    end practicee 20

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,36 @@
 import React from "react";
-import s from './Dialogs.module.css';
-
-
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-    return (
-        <div>
-            Dialogs
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className="dialog">
+            Dimych
         </div>
-    )
-}
+        <div className="dialog">
+            Andrey
+        </div>
+        <div className="dialog">
+            Sveta
+        </div>
+        <div className="dialog">
+            Sasha
+        </div>
+        <div className="dialog">
+            Viktor
+        </div>
+        <div className="dialog">
+            Valera
+        </div>
+      </div>
+    <div className="messages">
+        <div className="message">Hi</div>
+        <div className="message">How are you</div>
+        <div className="message">Yo</div>
+    </div>
+    </div>
+  );
+};
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import s from './Navbar.module.css';
+import s from "./Navbar.module.css";
 
 const Navbar = () => {
-    return  <nav className={s.nav}>
-    <div className={s.item}>
-      <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
-    </div>
-    <div className={`${s.item} ${s.active}`}>
-      <NavLink to="/dialogs">Messages</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink to="/news">News</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink to="/music">Music</NavLink>
-    </div>
-    <div className={s.item}>
-      <NavLink to="/settings">Settings</NavLink>
-    </div>
-  </nav>
-}
+  const activeLinkHandler = ({ isActive }) => {
+    return isActive ? s.active : "";
+  };
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" className={activeLinkHandler}>
+          Profile
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/dialogs" className={activeLinkHandler}>
+          Messages
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/news" className={activeLinkHandler}>
+          News
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to="/music" className={activeLinkHandler}>
+          Music
+        </NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink
+          to="/settings"
+          className={({ isActive }) => (isActive ? `${s.active}` : "")}
+        >
+          Settings
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
