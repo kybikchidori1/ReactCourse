d653edf35abbf8afdc8495ef332f715bf06ba3eb
Roman <kybik_rybik1994@mail.ru>
2022-05-01 17:46:27 +0300

    end practice 30

---
+++ src/redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, message: "Hi, How are you?", likesCount: 12 },
+      { id: 2, message: "Its my first post", likesCount: 13 },
+    ],
+  },
+  dialogsPage: {
+    dialogs: [
+      { id: 1, name: "Dimych" },
+      { id: 2, name: "Andrew" },
+      { id: 3, name: "Sveta" },
+      { id: 4, name: "Dio" },
+      { id: 5, name: "Loh" },
+      { id: 6, name: "Admiral" },
+    ],
+    messages: [
+      { id: 1, message: "hi" },
+      { id: 2, message: "how are you" },
+      { id: 3, message: "yo" },
+      { id: 4, message: "yo" },
+      { id: 5, message: "yo" },
+    ],
+  },
+  sidebar: {},
+};
+
+export default state;
