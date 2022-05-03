a348345133ba7e595ee78a61a74127071cd946e7
Roman <kybik_rybik1994@mail.ru>
2022-05-03 21:07:10 +0300

    add avatar

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,6 +4,7 @@ import s from "./Post.module.css";
 const Post = (props) => {
   return (
     <div className={s.item}>
+      <img src="https://i.pinimg.com/originals/0b/3f/74/0b3f74a151b808a193820874ba3bc555.jpg"></img>
       {props.message}
       <div>
         <span>like</span> {props.likesCount}
