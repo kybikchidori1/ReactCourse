cfa8bc077bb6efcff9e6e76607e4e190b980421a
Roman <kybik_rybik1994@mail.ru>
2022-05-03 12:29:56 +0300

    end practice 35

--- src/App.js
+++ src/App.js
@@ -7,41 +7,35 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route
-              path="/dialogs/*"
-              element={
-                <Dialogs
-                  state={props.state.dialogsPage}
-                  state={props.state.dialogsPage}
-                />
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <Profile
-                  state={props.state.profilePage}
-                  addPost={props.addPost}
-                />
-              }
-            />
-            <Route path="/news" element={<News />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Routes>
+          <Route
+            path="/dialogs/*"
+            element={<Dialogs state={props.state.dialogsPage} />}
+          />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                profilePage={props.state.profilePage}
+                addPost={props.addPost}
+                updateNewPostText={props.updateNewPostText}
+              />
+            }
+          />
+          <Route path="/news" element={<News />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,9 +1,12 @@
+import { rerenderEntireTree } from "../render.js";
+
 let state = {
   profilePage: {
     posts: [
       { id: 1, message: "Hi, How are you?", likesCount: 12 },
       { id: 2, message: "Its my first post", likesCount: 13 },
     ],
+    newPostText: "it-kamasutra.com",
   },
   dialogsPage: {
     dialogs: [
@@ -25,13 +28,22 @@ let state = {
   sidebar: {},
 };
 
-export let addPost = (postMessage) => {
+window.state = state;
+
+export let addPost = () => {
   let newPost = {
     id: 5,
-    message: postMessage,
+    message: state.profilePage.newPostText,
     likesCount: 0,
   };
   state.profilePage.posts.push(newPost);
+  state.profilePage.newPostText = "";
+  rerenderEntireTree(state);
+};
+
+export let updateNewPostText = (newText) => {
+  state.profilePage.newPostText = newText;
+  rerenderEntireTree(state);
 };
 
 export default state;
