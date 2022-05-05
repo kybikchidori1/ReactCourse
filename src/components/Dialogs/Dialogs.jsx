76ac7e037e95ae3e523e57fa5bbb6ae0b8a863a1
Roman <kybik_rybik1994@mail.ru>
2022-05-05 17:59:27 +0300

    end practice 41

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,7 @@ import Message from "./Message/Message.jsx";
 import {
   updateNewMessageBodyCreator,
   sendMessageCreator,
-} from "../../redux/state";
+} from "../../redux/dialogs-reducer.js";
 
 const Dialogs = (props) => {
   let state = props.store.getState().dialogsPage;
