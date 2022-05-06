1cfaa60acea455452d10a459e0dafea89d358b55
Roman <kybik_rybik1994@mail.ru>
2022-05-06 18:29:29 +0300

    end practice 43

---
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+import {
+  updateNewPostTextActionCreator,
+  addPostActionCreator,
+} from "../../../redux/profile-reducer.js";
+
+const MyPostsContainer = (props) => {
+  let postsElements = props.posts.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    // props.addPost();
+    props.dispatch(addPostActionCreator());
+  };
+
+  let onPostChange = () => {
+    let text = newPostElement.current.value;
+    // props.updateNewPostText(text);
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch(action);
+  };
+
+  return (
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
+      <div>
+        <div>
+          <textarea
+            onChange={onPostChange}
+            ref={newPostElement}
+            value={props.newPostText}
+          />
+        </div>
+        <div>
+          <button onClick={addPost}>Add post</button>
+        </div>
+        <div>
+          <button>Remove</button>
+        </div>
+      </div>
+      <div className={s.posts}>{postsElements}</div>
+    </div>
+  );
+};
+
+export default MyPostsContainer;
