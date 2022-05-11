7408c99298a5298f35aa1521a4368e5ec038c0a6
Roman <kybik_rybik1994@mail.ru>
2022-05-11 15:23:53 +0300

    end practice 61

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,6 +9,10 @@ const Header = () => {
         src="https://i.pinimg.com/originals/f6/7a/3a/f67a3a261f0c952640f9c1d214d73e96.png"
         alt=""
       />
+      <div className={s.loginBlock}>
+        {" "}
+        <NavLink to={"/login"}>Login</NavLink>
+      </div>
     </header>
   );
 };
